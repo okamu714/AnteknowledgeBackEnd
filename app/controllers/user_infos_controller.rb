46176b35162f9c26a4d277153193bcfdf34f195d
okamu714 <ryohei714ok@gmail.com>
2024-12-02 00:28:44 +0900

    一部変更

--- app/controllers/user_infos_controller.rb
+++ app/controllers/user_infos_controller.rb
@@ -1,19 +1,16 @@
 class UserInfosController < ApplicationController
   def index
-    user_infos = UserInfo.all
-    render json: user_infos
+    @user_infos = UserInfo.all
+    render json: @user_infos
   end
 
-  # ユーザーを登録する
   def create
-    # ユーザーが既に存在するか確認
-    user_info = UserInfo.find_by(user_id: params[:user_id])
+    user_info = UserInfo.find_by(user_id: user_info_params[:user_id])
 
     if user_info
       render json: { message: 'User already exists. No further action taken.' }, status: :ok
     else
-      # 新しいユーザーを作成
-      user_info = UserInfo.new(user_id: params[:user_id], email: params[:email])
+      user_info = UserInfo.new(user_info_params)
 
       if user_info.save
         render json: { message: 'User info saved successfully.' }, status: :ok
@@ -23,15 +20,18 @@ class UserInfosController < ApplicationController
     end
   end
 
-  # ユーザーの情報を見せる
   def show
-    def show
-      @user_info = UserInfo.find_by(user_id: params[:id])
-      if @user_info
-        render json: @user_info, status: :ok
-      else
-        render json: { error: "User not found" }, status: :not_found
-      end
+    @user_info = UserInfo.find_by(user_id: params[:id])
+    if @user_info
+      render json: @user_info, status: :ok
+    else
+      render json: { error: "User not found" }, status: :not_found
     end
+  end
+
+  private
+
+  def user_info_params
+    params.require(:user_info).permit(:user_id, :email)
   end
 end
